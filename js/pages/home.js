cde53229cf3b1335c17fd2647e28070f89bbbe4a
Linas <ketleriuslinas@gmail.com>
2025-05-28 18:25:20 +0300

    conflic resolved

--- js/pages/home.js
+++ js/pages/home.js
@@ -1,3 +1,9 @@
 
+import { header } from "../components/header.js";
+header(true);
+
+import { section2 } from "../components/section2.js";
+section2 ();
+
 import { section3 } from "../components/section3.js";
 section3 ();
