fdcd2ac669337c5e27e627cba4f69e8d1618defe
Linas <ketleriuslinas@gmail.com>
2025-05-30 12:28:14 +0300

    section-5 static

--- js/components/section5.js
+++ js/components/section5.js
@@ -1,44 +1,49 @@
+import { section5Data } from "../data/section5-data.js";
+
+
 export function section5() {
-    let HTML = `<div class="container bg-container">
+
+    let s5cardsHTML = '';
+
+    for (let i = 0; i < section5Data.length; i++) {
+        let sectionCards = section5Data[i];
+        s5cardsHTML += `
+            <div class="col-12 col-md-6 col-lg-6 col-xl-4 s5-card ">
+                <img class="s5-foto" src="./img/section-5/${sectionCards.img}" alt="${sectionCards.imgAlt}">
+                <div class="col-12  s5-content">
+                    <a href="${sectionCards.link}">
+                        <h2>${sectionCards.title}</h2>
+                    </a>
+                    <p>${sectionCards.description}</p>
+                </div>
+            </div>`;
+        }
+        
+        
+    const HTML = `    
+    <div class="container bg-container">
         <div class="row">
             <h1 class="s5-title col-12">Explore our recent projects</h1>
-            <div class="col-12 col-md-6 col-lg-6 col-xl-4 s5-card ">
-                <img class="s5-foto" src="./img/section-5/Digital-product.webp" alt="Digital-product-design">
-                <div class="col-12  s5-content">
-                    <h2>Digital product design</h2>
-                    <p>Design, graphics</p>
-                </div>
-            </div>
-            <div class="col-12 col-md-6 col-lg-6 col-xl-4 s5-card ">
-                <img class="s5-foto" src="./img/section-5/Cyber-security.webp" alt="Cyber Security Analysis">
-                <div class="col-12 s5-content">
-                    <h2>Cyber security analysis</h2>
-                    <p>Security, technology</p>
-                </div>
-            </div>
-            <div class="col-12 col-md-6 col-lg-6 col-xl-4 s5-card ">
-                <img class="s5-foto" src="./img/section-5/health-app.webp" alt="Health App Development">
-                <div class="col-12 s5-content">
-                    <h2>Health app development</h2>
-                    <p>Development, software</p>
-                </div>
-            </div>
-            <div class="col-12 col-md-6 col-lg-6 col-xl-4 s5-card ">
-                <img class="s5-foto " src="./img/section-5/Marketing-agency.webp" alt="Marketing Agency Website">
-                <div class="col-12 s5-content">
-                    <h2>Marketing agency website</h2>
-                    <p>Development, marketing</p>
-                </div>
-            </div>
+
+            ${s5cardsHTML}
+
             <div class="col-12 col-xl-8 s5-card ">
                 <img class="s5-foto" src="./img/section-5/project-marketing.webp" alt="Project for marketing">
                 <div class="col-12 s5-content">
-                    <h2>Project for marketing</h2>
+                    <a href="#">
+                        <h2>Project for marketing</h2>
+                    </a>
                     <p>Marketing, buisness</p>
                 </div>
+            </div>
+            <div class="row s5-btn">
+                <a class="col-12 m-3 col-lg-6 m-lg-5  m-md-4 " href="#">View our all works</a>
             </div>
         </div>
     </div>`;
 
-    document.getElementById('app').insertAdjacentHTML('beforeend', HTML);
+    document
+        .getElementById('app')
+        // .insertAdjacentElement('beforeend', s5cardsHTML);
+        .insertAdjacentHTML('beforeend', HTML);
 };
