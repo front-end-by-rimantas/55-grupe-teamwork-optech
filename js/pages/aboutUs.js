429aed4f269d907bd9b6b5d5f174a1154b274e16
romasmv <rmorozovas@gmail.com>
2025-06-01 16:01:17 +0300

    update  aboutus-section5

--- js/pages/aboutUs.js
+++ js/pages/aboutUs.js
@@ -1,7 +1,9 @@
 import { header } from "../components/header.js";
 import { aboutussection3 } from "../components/aboutUs-JS/aboutus-section3.js";
+import { aboutussection5 } from "../components/aboutUs-JS/aboutus-section5.js";
 import { footer} from "../components/footer.js";
 
 header();
 aboutussection3()
+aboutussection5()
 footer ();
