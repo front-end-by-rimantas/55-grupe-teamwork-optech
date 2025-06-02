62c0c288f86ef1f36beac76f353ac2f98ced95cb
romasmv <rmorozovas@gmail.com>
2025-06-02 19:11:17 +0300

    up

--- js/components/pricing-JS/pricing-section3.js
+++ js/components/pricing-JS/pricing-section3.js
@@ -1,43 +1,29 @@
+import { pricingsection3Data } from "../../data/prising-Data/pricing-section3Data.js";
+
 export function pricingsection3() {
+       let cardspricingsection3HTML = '';
+    
+       for (let i = 0; i < 4; i++) {
+          const pricingsection3 = pricingsection3Data[i];
+          cardspricingsection3HTML  += `
+                    <div class="pricingsec3">
+                        <div class="prcsec34-inside">
+                            <h5>${pricingsection3.h5}</h5>
+                            <button class="fa fa-plus"></button>
+                        </div>
+                        <p>${pricingsection3.p}</p>
+                    </div> `
+       }
 
     let HTML =  `
-       <div class="container back">
+        <div class="container back">
             <div class="row aboutus-sec5">
                 <div class="left-column-section4 col-12 sm-col-6 aboutus-sec5-inside">
                     <h1 class="title">Have any questions? here some answers</h1>
                     <p class="aboutus-section5-p">Each demo built with Teba will look different. You can customize almost anything in the appearance of your website with only</p>
                     <a href="#" class="btn">Ask Any Question</a>                  
                 </div>
-                <div class="right-column-prcsec34 col-12 sm-col-6">
-                    <div>
-                        <div class="prcsec34-inside">
-                            <h5>Q1. What Is The Design Process For Branding?</h5>
-                            <button class="fa fa-plus"></button>
-                        </div>
-                        <p>Each demo built with Teba will look different. You can customize almost anything in the appearance of your website with only a few clicks. Each demo built with Teba will look different.</p>
-                    </div>
-                    <div>
-                        <div class="prcsec34-inside">
-                            <h5>Q2. How Much Does Logo Design Services Cost?</h5>
-                            <button class="fa fa-plus"></button>
-                        </div>
-                        <p>Each demo built with Teba will look different. You can customize almost anything in the appearance of your website with only a few clicks. Each demo built with Teba will look different.</p>
-                    </div>
-                    <div>
-                        <div class="prcsec34-inside">
-                            <h5>Q3. How Long Will It Take To Complete My Project?</h5>
-                            <button class="fa fa-plus"></button>
-                        </div>
-                        <p>Each demo built with Teba will look different. You can customize almost anything in the appearance of your website with only a few clicks. Each demo built with Teba will look different.</p>
-                    </div>
-                    <div>
-                        <div class="prcsec34-inside">
-                            <h5>Q4. What Is Included In A Round Of Revisions?</h5>
-                            <button class="fa fa-plus"></button>
-                        </div>
-                        <p>Each demo built with Teba will look different. You can customize almost anything in the appearance of your website with only a few clicks. Each demo built with Teba will look different.</p>
-                    </div>
-                </div>
+                <div class="right-column-prcsec34 col-12 sm-col-6">${cardspricingsection3HTML}</div>
             </div>
         </div>`
 
