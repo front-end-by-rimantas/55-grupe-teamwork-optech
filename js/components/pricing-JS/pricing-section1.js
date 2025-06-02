40e52deac80d9a8f240fe55c4be9faae609b4cf0
romasmv <rmorozovas@gmail.com>
2025-06-02 15:35:54 +0300

    update pricing-section1

--- js/components/pricing-JS/pricing-section1.js
+++ js/components/pricing-JS/pricing-section1.js
@@ -1,13 +1,13 @@
-export function aboutussection1() {
+export function pricingsection1() {
 
     let HTML =  `
     <div class="aboutus-section1">
          <div class="aboutus-section1-inside abt-sec1-in2">
-            <h1>About Us</h1>
+            <h1>Pricing Plan</h1>
             <div class="aboutUs-sec1">
                 <a href="../">Home</a>
                 <P class="fa fa-chevron-right"></P>
-                <P href="#">About Us</P>
+                <P href="#">Pricing Plan</P>
             </div>
         </div>
     </div>`
