3b648b0411325c66ba80579dba78d2b45b25787e
romasmv <rmorozovas@gmail.com>
2025-06-02 17:15:01 +0300

    apdate pricing-section3

---
+++ js/components/pricing-JS/pricing-section3.js
@@ -0,0 +1,7 @@
+export function pricingsection3() {
+
+    let HTML =  `
+`
+
+    document.getElementById('app').insertAdjacentHTML('beforeend', HTML);
+}
--- js/pages/pricing.js
+++ js/pages/pricing.js
@@ -1,7 +1,9 @@
 import { header } from "../components/header.js";
 import { pricingsection1 } from "../components/pricing-JS/pricing-section1.js";
+import { pricingsection3 } from "../components/pricing-JS/pricing-section3.js";
 import { footer} from "../components/footer.js";
 
 header();
 pricingsection1()
+pricingsection3()
 footer ();
