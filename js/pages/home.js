5b2ea5e1ba300f335c10353e739f83ce9738754f
Cyber9933 <linate.pa@gmail.com>
2025-06-03 14:26:59 +0300

    secion9

--- js/pages/home.js
+++ js/pages/home.js
@@ -2,15 +2,19 @@
 import { header } from "../components/header.js";
 import { section2 } from "../components/section2.js";
 import { section3 } from "../components/section3.js";
+import { section6 } from "../components/section6.js";
 import {section7} from "../components/section7.js";
+import {section9} from "../components/section9.js";
 import { footer} from "../components/footer.js";
 
 header(true);
 section2 ();
 section3 ();
+section6 ();
 section7 ();
+section9 ();
 footer ();
 
-import { section6 } from "../components/section6.js";
-section6();
+
+
 
