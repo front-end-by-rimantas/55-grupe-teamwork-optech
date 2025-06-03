f52733109eb5d318939f9e8a5779f0e3fe1d4999
Aura-Lem <a.pozeraite@gmail.com>
2025-06-03 12:50:40 +0300

    about us section 2 static

--- js/pages/aboutUs.js
+++ js/pages/aboutUs.js
@@ -1,11 +1,13 @@
 import { header } from "../components/header.js";
 import { aboutussection1 } from "../components/aboutUs-JS/aboutus-section1.js";
+import { aboutUsSection2 } from "../components/aboutUs-JS/aboutus-section2.js";
 import { aboutussection3 } from "../components/aboutUs-JS/aboutus-section3.js";
 import { aboutussection5 } from "../components/aboutUs-JS/aboutus-section5.js";
 import { footer} from "../components/footer.js";
 
 header();
 aboutussection1();
+aboutUsSection2();
 aboutussection3();
 aboutussection5();
 footer ();
