ac1397d179d2cfc083aee1360b75c633e08db574
Aura-Lem <a.pozeraite@gmail.com>
2025-06-04 13:48:41 +0300

    service section1

--- js/pages/service.js
+++ js/pages/service.js
@@ -1,8 +1,10 @@
 import { header } from "../components/header.js";
+import { serviceSection1 } from "../components/service/service-section1.js";
 import { serviceSection2 } from "../components/service/service-section2.js";
 
 import { footer} from "../components/footer.js";
 
 header();
+serviceSection1();
 serviceSection2();
 footer ();
