7790524b6c7db7175fa83047e39fcf033339a33a
Aura-Lem <a.pozeraite@gmail.com>
2025-06-05 13:20:25 +0300

    service details naujas puslapis

---
+++ js/pages/service-details.js
@@ -0,0 +1,9 @@
+import { header } from "../components/header.js";
+
+import { aboutussection6 } from "../components/aboutUs-JS/aboutus-section6.js";
+import { footer} from "../components/footer.js";
+
+header();
+
+aboutussection6()
+footer ();
