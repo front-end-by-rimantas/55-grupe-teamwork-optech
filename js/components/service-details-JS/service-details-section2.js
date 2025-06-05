6d6898236bb2dce6d28935af5c56083bd0113ceb
Aura-Lem <a.pozeraite@gmail.com>
2025-06-05 15:26:58 +0300

    service details section 2 nauji failai

---
+++ js/components/service-details-JS/service-details-section2.js
@@ -0,0 +1,11 @@
+export function serviceDetailsSection2 () {
+
+    let HTML =  `
+    <div class="container">
+        <div class="row">
+            
+        </div>
+    </div>`
+
+    document.getElementById('app').insertAdjacentHTML('beforeend', HTML);
+}
