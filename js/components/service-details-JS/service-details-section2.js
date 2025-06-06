8d6b9874cc6ed18efad840777296d721f1842194
Aura-Lem <a.pozeraite@gmail.com>
2025-06-06 10:27:48 +0300

    responsive d

--- js/components/service-details-JS/service-details-section2.js
+++ js/components/service-details-JS/service-details-section2.js
@@ -3,15 +3,15 @@ export function serviceDetailsSection2 () {
     let HTML =  `
     <div class="container">
         <div class="row">
-            <div class="col-12">
-                <img class="serviceD-photo" src="../../img/service-details-section2/service-deatils.webp" alt="work"/>
+            <div class="col-12 col-xxl-8 serviceD-left">
+                    <img class="serviceD-photo" src="../../img/service-details-section2/service-deatils.webp" alt="work"/>
                 <div class="serviceD-content">
-                    <h4 class="serviceD-title">Overview</h4>
+                    <h3 class="serviceD-title">Overview</h3>
                     <p class="serviceD-text">A content management system helps you create, manage, and publish content on the web. It also keep content organized and accessible so it can be used and repurposed effectively. There are various kinds of content management systems available—from cloud-based to a headless</p>
                     <p class="serviceD-text">CMS provides user-friendly features for easy editing and is compatible with installing plugins and tools that provide even more features for advanced functions. API CMS’s are built to have an excellent user-friendly interface that is easy to use.</p>
                 </div>
                 <div class="serviceD-content">
-                    <h4 class="serviceD-title">Features</h4>
+                    <h3 class="serviceD-title">Features</h3>
                     <p class="serviceD-text">A content management system (CMS) is an application that is used to manage content, allowing multiple contributors to create, edit and publish. Content in a CMS is typically stored in a database and displayed in a presentation layer based on a set of templates like a website.</p>
                     <div class="serviceD-list">
                         <i class="fa2 fa fa-check"></i>
@@ -27,13 +27,13 @@ export function serviceDetailsSection2 () {
                     </div>
                 </div>
                 <div class="serviceD-content">
-                    <h4 class="serviceD-title">Goal</h4>
+                    <h3 class="serviceD-title">Goal</h3>
                     <p class="serviceD-text">A content management system (CMS) is an application that is used to manage content, allowing multiple contributors to create, edit and publish. Content in a CMS is typically stored in a database and displayed in a presentation layer based on a set of templates like a website.</p>
                 </div>
             </div>
-            <div class="serviceD-links">
-                <div>
-                    <div>
+            <div class="col-12 col-xxl-4 serviceD-links">
+                <div class="serviceD-sidebar">
+                    <div class="serviceD-sidebar-menu">
                         <ul>
                             <li>
                                 <a href="#">Web Development<i></i></a>
