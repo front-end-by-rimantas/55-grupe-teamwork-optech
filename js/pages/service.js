b41f7ae6528da794bc97d8147679ef6259b712f1
Linas <ketleriuslinas@gmail.com>
2025-06-06 10:18:42 +0300

    static design/index.html

--- js/pages/service.js
+++ js/pages/service.js
@@ -1,10 +1,13 @@
 import { header } from "../components/header.js";
 import { serviceSection1 } from "../components/service/service-section1.js";
 import { serviceSection2 } from "../components/service/service-section2.js";
+// import { serviceSection3 } from "../components/service/service-section3.js";
 
-import { footer} from "../components/footer.js";
+
+import { footer } from "../components/footer.js";
 
 header();
 serviceSection1();
 serviceSection2();
-footer ();
+serviceSection3();
+footer();
