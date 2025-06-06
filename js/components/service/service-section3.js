4a859708b0065be8867efd49b9b6a207b44ee6d8
Linas <ketleriuslinas@gmail.com>
2025-06-06 14:18:50 +0300

    service section 3 static design, responsive +,

--- js/components/service/service-section3.js
+++ js/components/service/service-section3.js
@@ -1,4 +1,4 @@
-import { section3Data } from "../../data/section3Data";
+import { section3Data } from "../../data/section3Data.js";
 
 export function serviceSection3() {
 
@@ -7,25 +7,28 @@ export function serviceSection3() {
     for (let i = 0; i < section3Data.length; i++) {
         let serviceSectionCards = section3Data[i];
         s3cardsHTML += `
-            <div class="s3-column col-12 col-md-6 col-xl-4 col-xxl-3 row-cards">
-                <div class="s3-icons">
-                    <img src="./img/section-3/${serviceSectionCards.img}" alt="${serviceSectionCards.imgAlt}">
+            <div class="service-s3-column  row-cards col-12">
+                <div class="service-s3-icons">
+                    <img class="s3-img-color" src="../img/section-3/${serviceSectionCards.img}" alt="${serviceSectionCards.imgAlt}">
                 </div>
-                <h2 class="clm-title">${serviceSectionCards.title}</h2>
-                <p class="clm-p">${serviceSectionCards.description}</p>
-                <a class="clm-a" href="${serviceSectionCards.link}">Learn More</a>
-            </div>
-        `;
+                <div>
+                    <h2 class="service-clm-title">${serviceSectionCards.title}</h2>
+                    <p class="clm-p">${serviceSectionCards.description}</p>
+                    <a class="clm-a " href="${serviceSectionCards.link}">Learn More</a>
+                </div>
+            </div>`;
     }
 
 
     let HTML = `
-<div class="container bg-container">
+    <div class="container bg-container">
         <div class="row">
-            <h1 class="s3-title col-12 col-lg-8 m-md-1 m-lg-3">Our awesome services to give you success</h1>
-        </div>
-
-        ${s3cardsHTML}`;
+            <h1 class="service-s3-title col-12 col-lg-6 m-md-1 m-lg-3 s3-titleMD">Our awesome services to give you
+                success</h1>
+         </div>
+         <div class="row">
+         ${s3cardsHTML}
+        </div>`;
 
 
     document
--- js/pages/service.js
+++ js/pages/service.js
@@ -1,9 +1,7 @@
 import { header } from "../components/header.js";
 import { serviceSection1 } from "../components/service/service-section1.js";
 import { serviceSection2 } from "../components/service/service-section2.js";
-// import { serviceSection3 } from "../components/service/service-section3.js";
-
-
+import { serviceSection3 } from "../components/service/service-section3.js";
 import { footer } from "../components/footer.js";
 
 header();
