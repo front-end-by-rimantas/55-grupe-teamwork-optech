489410263a1e64cc32840c0da5f9c4fb61b245be
Cyber9933 <linate.pa@gmail.com>
2025-06-07 14:24:15 +0300

    pages-service-service#4

--- js/pages/service.js
+++ js/pages/service.js
@@ -2,6 +2,7 @@ import { header } from "../components/header.js";
 import { serviceSection1 } from "../components/service/service-section1.js";
 import { serviceSection2 } from "../components/service/service-section2.js";
 import { serviceSection3 } from "../components/service/service-section3.js";
+import { serviceSection4 } from "../components/service/service-section4.js";
 import { aboutussection6 } from "../components/aboutUs-JS/aboutus-section6.js";
 import { footer} from "../components/footer.js";
 
@@ -10,6 +11,7 @@ header();
 serviceSection1();
 serviceSection2();
 serviceSection3();
+serviceSection4();
 aboutussection6();
 footer();
 
