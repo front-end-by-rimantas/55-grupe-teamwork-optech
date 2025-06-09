58889be2330d13e5f8a10317789ce1fa7bc412a3
romasmv <rmorozovas@gmail.com>
2025-06-09 15:34:13 +0300

    update strukture

---
+++ js/pages/contact.js
@@ -0,0 +1,9 @@
+import { header } from "../components/header.js";
+import { aboutussection1 } from "../components/aboutUs-JS/aboutus-section1.js";
+import { aboutussection5 } from "../components/aboutUs-JS/aboutus-section5.js";
+import { footer} from "../components/footer.js";
+
+header();
+aboutussection1()
+aboutussection5()
+footer ();
